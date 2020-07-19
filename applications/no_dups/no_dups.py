50c9a97c5954875cce8153a2312841ed9351f977
Jeremy Morales <jeremy.morales@gmail.com>
2020-07-19 14:07:53 -0700

    no_dups completed

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,29 @@
 def no_dups(s):
     # Your code here
+    new_string = ""
 
+    if len(s) == 0:
+        return new_string
+    else:
+
+        uncounted = s.split()
+        found_words = dict.fromkeys(uncounted)
+
+        # print(uncounted)
+
+        # print(list(found_words))
+        step = 1
+        for key in list(found_words):
+            if step == 1:
+                new_string = key
+            else:
+                new_string = new_string + " " + key
+            step += 1
+        return new_string
+
+        # for word in uncounted:
+        #     if word not in found_words:
+        #         found_words[word] = 1
 
 
 if __name__ == "__main__":
