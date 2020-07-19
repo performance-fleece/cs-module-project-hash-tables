7b6191a660ad1759da96e4e97dc96e252c8543e2
Jeremy Morales <jeremy.morales@gmail.com>
2020-07-19 14:08:29 -0700

    removed print statements

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -5,14 +5,10 @@ def no_dups(s):
     if len(s) == 0:
         return new_string
     else:
-
         uncounted = s.split()
         found_words = dict.fromkeys(uncounted)
-
-        # print(uncounted)
-
-        # print(list(found_words))
         step = 1
+
         for key in list(found_words):
             if step == 1:
                 new_string = key
@@ -20,10 +16,6 @@ def no_dups(s):
                 new_string = new_string + " " + key
             step += 1
         return new_string
-
-        # for word in uncounted:
-        #     if word not in found_words:
-        #         found_words[word] = 1
 
 
 if __name__ == "__main__":
